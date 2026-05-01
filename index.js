fc8102cc610c4ea2af78048f5cf7f00e07b4fd39
Subransu <maharanasubhransu6@gmail.com>
2026-05-01 16:48:18 +0530

    Add Initial Setup

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 import http from 'http';
+import express from 'express';
+import dotenv from 'dotenv';
+import path from 'node:path';
+
+dotenv.config();
+
+const app = express();
+app.use(express.static(path.resolve("./public")));
+
+const PORT = process.env.PORT || 9000;
+
+
 
 
 async function main(){
-    const server = http.createServer();
+    const server = http.createServer(app);
+
+    server.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    })
 }
 
 main();
